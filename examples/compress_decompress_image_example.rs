68605f688ad1ececeb182c5e9f8557026d938df4
Voggo <nielsviggostark@gmail.com>
2026-04-13 13:37:20 +0200

    config

--- examples/compress_decompress_image_example.rs
+++ examples/compress_decompress_image_example.rs
@@ -10,7 +10,7 @@ use std::path::Path;
 fn main() -> Result<(), EntroGdError> {
     unsafe {
         env::set_var("ENTRO_GD_LOG_TO_STDERR", "1");
-        env::set_var("RUST_LOG", "trace");
+        env::set_var("RUST_LOG", "info");
     }
     let _log_handle = init_logging();
     let _timer =
@@ -69,14 +69,13 @@ fn main() -> Result<(), EntroGdError> {
     let compression_pipeline = BuildImageBitDataSet {
         colorspace: ImageColorSpace::SrgbWithLinearAlpha,
         color_model: ImageColorModel::YCoCgR,
-        pixel_grouping: 2,
+        pixel_grouping: 4,
         grouping_transform: ImageGroupingTransform::ForFirstPixel,
         pad_rows_to_word: DEFAULT_ALIGN_ROWS_TO_WORD,
     }
     .then(EntropyNaive {})
-    .then(SelectBasesProfileAllBits {
-        split_into_batches: 1,
-        base_bit_impl: BaseBitImpl::Naive,
+    .then(SelectBases {
+        patience: 10,
     })
     .then(EncodeDataFusedDictionary {});
 
